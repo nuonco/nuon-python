b55724ec4d4a5c16de19404e40aaca29e61cf6b1
fidiego <sayheytofred@gmail.com>
2025-10-14 10:18:19 -0500

    chore: update cleanup script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,1 +1,4 @@
-rm -r version.txt .openapi-generator docs nuon pyproject.toml requirements.txt setup.cfg setup.py test-requirements.txt test tox.ini dist
+rm -rf \
+  version.txt \
+  nuon        \
+  dist
